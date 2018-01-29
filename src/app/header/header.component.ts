ee4d0394e197460f06d869324ab3d8d47534f65d
dugls <rgasiy12@gmail.com>
2018-01-29 21:35:39 +0200

    fadf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, } from '@angular/core';
-import { trigger,state,style,transition,animate,keyframes, query, group} from '@angular/animations';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
+import { trigger,state,style,transition,animate,keyframes, query, group, AnimationFactory} from '@angular/animations';
 import { TweenLite } from 'gsap'
 
 @Component({
@@ -7,7 +7,6 @@ import { TweenLite } from 'gsap'
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   animations: [
-
     trigger('fadeInOut', [
       state('*', style({
         opacity: 1
@@ -20,16 +19,35 @@ import { TweenLite } from 'gsap'
         animate(250, style({opacity:0})) 
       ])
   ]),
-    trigger('logoAnimate', [
-      state('*',style({
-        
-      }))
-    ])
+    trigger('navAnimate', [
+      state('off',style({
+          color: 'white',
+      })),
+      state('on', style({
+          color: 'black'
+      }
+      )),
+      transition('off => on', animate('300ms')),
+      transition('on => off', animate('300ms'))
+    ]),
+    trigger('barsDivAnimation', [
+       state('hide', style({
+          background: 'white'
+       })),
+       state('show', style({
+         background: 'black' 
+       })),
+      transition('off => on', animate('300ms')),
+      transition('on => off', animate('300ms'))
+      ])
+
   ]
 })
 export class HeaderComponent implements OnInit {
-  logo: string = "Угорська";
+
   state:boolean;
+  tog = 'off'
+  bars = 'hide'
 
   barsAnimate(){
     if(!this.state === true){
@@ -52,16 +70,37 @@ export class HeaderComponent implements OnInit {
       let list = document.getElementsByClassName('dropdown-list')
       TweenLite.fromTo(list, 0.6, {opacity: 1}, {opacity: 0})
     }
-  }  
+  } 
+
+
+  constructor(public el: ElementRef) {}
+
+  @HostListener('window:scroll', ['$event'])
+    checkNavScroll() {
+      const componentPosition = this.el.nativeElement.offsetTop
+
+      const scrollPosition = window.pageYOffset
+
+      if(scrollPosition > 680){
+        this.tog = 'on'
+        this.bars = 'show'
+      }
+      else{
+        this.tog = 'off'
+        this.bars = 'hide'
+      }
+    }  
 
   toggle(){  
     this.state = !this.state
   }
 
-    constructor() {}
+
 
     ngOnInit() {
 
     }
 
 }
+
+
